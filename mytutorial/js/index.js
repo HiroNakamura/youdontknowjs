b1e12e9cb70b1aaebf085fba9f0ee44966932195
Fer_Carraro <carraro.fernando@gmail.com>
2020-04-04 15:26:37 -0600

    Se agregaron mascambios

--- mytutorial/js/index.js
+++ mytutorial/js/index.js
@@ -1,14 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    
-    const TAM = 100;
-    const div0 = document.getElementById('div0');
-    
     console.log('You do not know Javascript !!');
 
-    if(div0 != null){
-        console.log('Elemento: ',div0)
-    }
+    const TAM = 100;
     
+    const btn1 = document.getElementById('muestra1');
+    const btn2 = document.getElementById('muestra2');
     
+    btn1.addEventListener('click',()=>{
+        if(btn1.textContent == 'Mostrar'){
+            document.getElementById("code1").removeAttribute("style");
+            btn1.textContent = 'Ocultar'
+        }
+        if(btn1.textContent == 'Ocultar'){
+            document.getElementById("code1").addAttribute("style");
+            document.getElementById('code1').style.display='none';
+            btn1.textContent = 'Mostrar'
+        }
+    });
+
+    btn2.addEventListener('click',()=>{
+        if(btn2.textContent == 'Mostrar'){
+            document.getElementById("code2").removeAttribute("style");
+            btn2.textContent = 'Ocultar'
+        }
+        
+        if(btn2.textContent == 'Ocultar'){
+            document.getElementById("code2").addAttribute("style");
+            document.getElementById('code2').style.display='none';
+            btn2.textContent = 'Mostrar'
+        }
+    });
     
 });
