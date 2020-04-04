a1ac27cfcf0062ee4d73f5fe8581f72b2478e329
Fer_Carraro <carraro.fernando@gmail.com>
2020-04-04 17:52:08 -0600

    Se edito index.js, index.css e index.html

--- mytutorial/js/index.js
+++ mytutorial/js/index.js
@@ -5,15 +5,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     const btn1 = document.getElementById('muestra1');
     const btn2 = document.getElementById('muestra2');
-    
+
     btn1.addEventListener('click',()=>{
         if(btn1.textContent == 'Mostrar'){
             document.getElementById("code1").removeAttribute("style");
             btn1.textContent = 'Ocultar'
-        }
-        if(btn1.textContent == 'Ocultar'){
-            document.getElementById("code1").addAttribute("style");
-            document.getElementById('code1').style.display='none';
+        }else{
+            document.getElementById("code1").setAttribute("style","display: none;");
             btn1.textContent = 'Mostrar'
         }
     });
@@ -23,12 +21,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
             document.getElementById("code2").removeAttribute("style");
             btn2.textContent = 'Ocultar'
         }
-        
-        if(btn2.textContent == 'Ocultar'){
-            document.getElementById("code2").addAttribute("style");
-            document.getElementById('code2').style.display='none';
+        else{
+            document.getElementById("code2").setAttribute("style","display: none;");
             btn2.textContent = 'Mostrar'
         }
     });
+
+
+    
     
 });
