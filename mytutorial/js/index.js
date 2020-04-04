7dd75eaf78cc416cdf255c1876f033c18ab7e2d7
Fer_Carraro <carraro.fernando@gmail.com>
2020-04-03 20:20:16 -0600

    Se edito index.js

--- mytutorial/js/index.js
+++ mytutorial/js/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    
+    const TAM = 100;
+    const div0 = document.getElementById('div0');
+    
     console.log('You do not know Javascript !!');
+
+    if(div0 != null){
+        console.log('Elemento: ',div0)
+    }
+    
+    
+    
 });
